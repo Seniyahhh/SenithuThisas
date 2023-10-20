2cd4c11ca7e7da167adfd36310d3c15b479ad092
Senithu Thisas <123149435+Seniyahhh@users.noreply.github.com>
2023-10-20 20:26:50 +0530

    Add files via upload

---
+++ resources/views/BiscuitPuddings.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Coco Gelato</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        <style>
+           
+               
+        </style>
+    </head>
+    <body class="flex center postion-ref full hight">
+        <div class="div">
+            <div class="content">
+                <div class="title m-b-md" align="center">
+                    <h1>Biscuit Puddings</h1>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
